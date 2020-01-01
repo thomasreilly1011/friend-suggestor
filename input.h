3bbd2129b8258dfd3677492e6f1e50e17a298469
thomasreilly1011 <thomas.edreilly@gmail.com>
2020-01-01 20:34:29 +0000

    Init commit

---
+++ input.h
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   input.h
+ * Author: thoma
+ *
+ * Created on February 1, 2019, 12:25 PM
+ */
+
+#ifndef INPUT_H
+#define INPUT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INPUT_H */
+
+
